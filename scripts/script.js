1fc2b3a8d913910840b044d8ba9290f6c30d6bf2
Anh Nguyen <anhnguyeen.ngoc@gmail.com>
2024-09-29 12:02:24 +0700

    fix: use createElement instead of innerHTML

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+const addedItems = [];
+
 const renderDessertItems = () => {
     const desserts = document.querySelector('#desserts')
 
@@ -8,33 +10,92 @@ const renderDessertItems = () => {
             const dessertItem = document.createElement('div');
             dessertItem.classList.add('card');
 
-            dessertItem.innerHTML = `
-                <div class="thumbnail">
-                    <picture>
-                        <source width="100%" media="(max-width:767px)" srcset="${dessert.image.mobile}">
-                        <source media="(min-width:1024px)" srcset="${dessert.image.desktop}">
-                        <img src="${dessert.image.tablet}" alt="${dessert.name}" style="width: auto; max-width: 100%">
-                    </picture>
-                    <button class="add-cart" onClick="${addToCart}">Add to Cart</button>
-                </div>
+            const thumbnail = document.createElement('div');
+            thumbnail.classList.add('thumbnail');
 
-                <h3>${dessert.category}</h3>
-                <h4>${dessert.name}</h4>
-                <h5>€${dessert.price.toFixed(2)}</h5>
+            const picture = document.createElement('picture');
 
+            const mobileImg = document.createElement('source');
+            mobileImg.setAttribute('width', '100%');
+            mobileImg.setAttribute('media', '(max-width:767px)');
+            mobileImg.setAttribute('srcset', dessert.image.mobile);
+            picture.appendChild(mobileImg);
 
-            `
+            const desktopImg = document.createElement('source');
+            desktopImg.setAttribute('media', '(min-width:1024px)');
+            desktopImg.setAttribute('srcset', dessert.image.desktop);
+
+            const defaultImg = document.createElement('img');
+            defaultImg.setAttribute('src', dessert.image.tablet);
+            defaultImg.style.width = "auto";
+            defaultImg.style.maxWidth = "100%";
+
+            picture.appendChild(mobileImg);
+            picture.appendChild(desktopImg);
+            picture.appendChild(defaultImg);
+
+            const addToCartBtn =  document.createElement('button');
+            addToCartBtn.classList.add('add-cart');
+            addToCartBtn.textContent = 'Add to Cart';
+
+            thumbnail.appendChild(picture);
+            thumbnail.appendChild(addToCartBtn);
+
+            const itemCategory = document.createElement('h3');
+            itemCategory.textContent = dessert.category;
+
+            const itemName = document.createElement('h4');
+            itemName.textContent = dessert.name;
+
+            const itemPrice = document.createElement('h5');
+            itemPrice.textContent = '€' + dessert.price.toFixed(2);
+
+            dessertItem.appendChild(thumbnail);
+            dessertItem.appendChild(itemCategory);
+            dessertItem.appendChild(itemName);
+            dessertItem.appendChild(itemPrice);
+
+            addToCartBtn.addEventListener('click', () => addToCart(dessert));
             desserts.appendChild(dessertItem);
         })
     })
 }
 
-const addToCart = () => {
-    console.log('addToCart')
+function addToCart(dessert) {
+    const existingItem = addedItems.find(item => item.name === dessert.category)
+
+    if (existingItem) {
+        existingItem.count++;
+    } else {
+        const dessertItem = {
+            name: dessert.category,
+            price: dessert.price,
+            count: 1,
+        }
+        addedItems.push(dessertItem);
+    }
+    renderCart();
 }
+
+function renderCart() {
+    const cart = document.getElementById('cart');
+
+    if (addedItems.length === 0) {
+        const message = document.createElement('p');
+        message.textContent = 'Your added items will appear here.';
+        cart.appendChild(message);
+    }
+
+    const cartQty = addedItems.reduce((sum, item) => {
+        return sum + item.count;
+      }, 0);
+    console.log("🚀 ~ cartQty ~ cartQty:", cartQty)
+}
+
 
 window.onload = () => {
     renderDessertItems();
+    renderCart();
 }
 
 
